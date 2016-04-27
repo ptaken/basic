649a432ff998b27bf97677fd7e30415ad2037e7f
pawana.p <pawana.p@pawana_NB.rbr.nhso.go.th>
2016-04-28 00:36:18 +0700

    frist

---
+++ views/first/page1.php
@@ -0,0 +1,3 @@
+<h1>My Frist Page1</h1>
+<hr />
+ฉันชื่อ Pawana Phunsan
---
+++ views/first/page2.php
@@ -0,0 +1,11 @@
+<?php
+
+echo $hello;
+echo "<hr />";
+echo $a;
+echo "<hr />";
+echo $b;
+echo "<hr />";
+echo $c;
+echo "<hr />";
+echo $a*$b;
